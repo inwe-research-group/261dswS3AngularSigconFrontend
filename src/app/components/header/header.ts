cca8e738f94562b53dd61ca3b6f83ba1d46c370f
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-04-21 18:57:34 -0500

    homepage end

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
